58586a621038db889dd691c8c5084af5043cca71
spr <spr@spr-laptop.(none)>
2010-05-04 17:25:34 +0200

    lista 9

--- lista9.hs
+++ lista9.hs
@@ -16,51 +16,29 @@ fibSum =
 -- zadanie 3
 
 maxSeqLen :: Integer
-maxSeqLen = maxSeqLen' limit 0 where 
-    limit = 10 ^ 4
-    seqLen prev len =
-        if prev == 1 then 
-            len
-        else
-            if even prev 
-                then seqLen (prev `div` 2) (len + 1)
-                else seqLen (3 * prev + 1) (len + 1)
-    maxSeqLen' 0 maxLen = maxLen
-    maxSeqLen' n maxLen = 
-        if len > maxLen
-            then maxSeqLen' (n - 1) len
-            else maxSeqLen' (n - 1) maxLen
-        where len = seqLen n 1
-                    
-maxSeqLen' :: Integer
-maxSeqLen' = maxSeqLen'' limit 0 0 0 limit where 
-    limit = 10 ^ 4
-    maxSeqLen'' _    _   _    maxlen 0 = maxlen
-    maxSeqLen'' prev len maxn maxlen n =
-        if prev == maxn then 
-            maxSeqLen'' (n - 1) 1 n (maxlen + len - 1) (n - 1)
-        else
-            if prev == 1 then
-                if len > maxlen 
-                    then maxSeqLen'' (n - 1) 1 n len (n - 1)
-                    else maxSeqLen'' (n - 1) 1 maxn maxlen (n - 1)           
-            else
-                if even prev 
-                    then maxSeqLen'' (prev `div` 2) (len + 1) maxn maxlen n
-                    else maxSeqLen'' (3 * prev + 1) (len + 1) maxn maxlen n
-  
+maxSeqLen = maxSeqLen' limit where 
+    limit = 10 ^ 6
+    seqLen 1 = 1
+    seqLen n
+        | even n = 1 + seqLen (n `div` 2)
+        | otherwise = 1 + seqLen (3 * n + 1)
+    maxSeqLen' 0 = 0
+    maxSeqLen' n
+        | len > maxLen = len
+        | otherwise = maxLen
+        where len = seqLen n
+              maxLen = maxSeqLen' (n - 1)
+                      
 
 -- zadanie 4
 
 digSum :: Integer
-digSum = digSum' 0 (fact number) where 
+digSum = digSum' (fact number) where 
     number = 100
     fact 0 = 1
     fact n = n * fact (n - 1)
-    digSum' acc n = 
-        if n == 0 
-            then acc
-            else digSum' (acc + (n `mod` 10)) (n `div` 10)
+    digSum' 0 = 0
+    digSum' n = (n `mod` 10) + digSum' (n `div` 10)
     
  
 -- zadanie 5
